f1c45b239dd5997c1c00dedd329ea0c488212cf2
tiagoprimo <tiagoprimo@gmail.com>
2017-07-10 14:24:53 -0300

    Second Version - Now with a function

---
+++ rename_files.py
@@ -0,0 +1,11 @@
+import os
+
+def rename_files(): #function creted
+    #(1) get file names from a folder
+    file_list = os.listdir("../udemystudy/images")
+    print(file_list)
+    #(2) for each file, rename the filename
+
+
+
+rename_files() #function called
